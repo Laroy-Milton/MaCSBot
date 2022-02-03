26612ad8a38e9bf1f76f5f7e194d320622829538
Laroy Milton <laroymilton@gmail.com>
2022-02-03 12:10:09 -0700

    Add post req from front end login to backend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 var path    = require("path");
 const app = new express();
 
@@ -18,10 +19,29 @@ const port = 5500;
 // });
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/test.html'));
+});
+// app.post('/', function(req, res){
+//   console.log(res);
+//   console.log(req);
+// });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+app.post('/', (req, res) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
 });
 
 app.listen(port, hostname,() => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  
 });
 
+
+// const urlParams = new URLSearchParams(queryString);
+
+// var userId = getUrlParam('userid','Empty');
+// console.log(`userID is: ${userID}`);
